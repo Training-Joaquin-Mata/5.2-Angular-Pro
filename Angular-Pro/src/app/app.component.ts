6d052217c681b48173c80e7c878995bd0f3a6977
Joaquin Mata <joaquinmata.a@gmail.com>
2021-09-28 15:19:34 -0500

    custom structural directive

--- Angular-Pro/src/app/app.component.ts
+++ Angular-Pro/src/app/app.component.ts
@@ -3,8 +3,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls:['./app.component.scss']
+  template: `
+  <div>
+    <ul>
+      <li *myFor="let item of items; let i = index;">
+        {{ i }} Member: {{ item.name | json }}
+      </li>
+      <template myFor [myForOf]="items" let-item let-i="index">
+        <li>
+          {{ i }} Member: {{ item.name | json }}
+        </li>
+      </template>
+    </ul>
+  </div>
+`
 })
 export class AppComponent {
-  title = 'Angular-Pro';
+  items = [{
+    name: 'Mark Hoppus',
+    age: 44,
+    location: 'California'
+  },{
+    name: 'Tom Delonge',
+    age: 41,
+    location: 'California'
+  },{
+    name: 'Travis Barker',
+    age: 41,
+    location: 'California'
+  }];
+  constructor() {
+    setTimeout(() => {
+      this.items = [...this.items, { name: 'Matt Skiba', age: 40, location: 'California' }];
+    }, 2000);
+}
 }
--- Angular-Pro/src/app/app.module.ts
+++ Angular-Pro/src/app/app.module.ts
@@ -4,9 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CreditCardDirective } from './credit-card/credit-card.directive';
+import { TooltipDirective } from './tooltip/tooltip.directive';
+import { MyForDirective } from './my-for/my-for.directive';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreditCardDirective,
+    TooltipDirective,
+    MyForDirective
   ],
   imports: [
     BrowserModule,
