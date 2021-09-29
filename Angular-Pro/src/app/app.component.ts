5142c4ab12201fb2b538bf750cc2c2317ba4030d
Joaquin Mata <joaquinmata.a@gmail.com>
2021-09-29 12:33:56 -0500

    FormBuilder API

--- Angular-Pro/src/app/app.component.ts
+++ Angular-Pro/src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <stock-inventory></stock-inventory>
+    </div>
+  `
 })
-export class AppComponent {
-  title = 'Angular-Pro';
-}
+export class AppComponent {}
+
