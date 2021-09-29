6732431e032f3953af73bc02ba9e834998b794f5
Joaquin Mata <joaquinmata.a@gmail.com>
2021-09-28 19:35:42 -0500

    pipes

--- Angular-Pro/src/app/app.component.ts
+++ Angular-Pro/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+
+import { FileSizePipe } from '../app/filesize.pipe'
+
+interface File {
+  name: string,
+  size: any,
+  type: string
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <div *ngFor="let file of mapped">
+        <p>{{ file.name }}</p>
+        <p>{{ file.size }}</p>
+      </div>
+    </div>
+  `,
+  providers: [ FileSizePipe ]
 })
-export class AppComponent {
-  title = 'Angular-Pro';
+export class AppComponent implements OnInit {
+  mapped: any = null;
+  files: File[]=[];
+  constructor(private fileSize: FileSizePipe){}
+  ngOnInit() {
+    this.files = [
+      { name: 'logo.svg', size: 2120109, type: 'image/svg' },
+      { name: 'banner.jpg', size: 18029, type: 'image/jpg' },
+      { name: 'background.png', size: 1784562, type: 'image/png' }
+    ];
+
+    this.mapped = this.files.map(file=>{ return {name: file.name, type: file.type, size: this.fileSize.transform(file.size, 'mb')} })
+
+  }
 }
