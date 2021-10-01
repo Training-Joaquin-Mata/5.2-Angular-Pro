d1c8c9ce6e1ea3894b5360d1465765fabfc1f431
Joaquin Mata <joaquinmata.a@gmail.com>
2021-10-01 18:55:07 -0500

    testing an attribute directive

--- Angular-Pro/karma.conf.js
+++ Angular-Pro/karma.conf.js
@@ -1,44 +1,54 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+const webpack = require('webpack');
 
-module.exports = function (config) {
+module.exports = (config) => {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
-    },
-    coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Angular-Pro'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    files: [
+      'node_modules/reflect-metadata/Reflect.js',
+      'node_modules/zone.js/dist/zone.js',
+      'node_modules/zone.js/dist/proxy.js',
+      'node_modules/zone.js/dist/sync-test.js',
+      'node_modules/zone.js/dist/async-test.js',
+      'node_modules/zone.js/dist/jasmine-patch.js',
+      'node_modules/zone.js/dist/long-stack-trace-zone.js',
+      { pattern: __dirname + '/**/*.spec.ts', watched: false }
+    ],
+    frameworks: ['jasmine'],
+    mime: { 'text/x-typescript': ['ts'] },
+    preprocessors: {
+      '*.js': ['sourcemap'],
+      '**/*.spec.ts': ['sourcemap', 'webpack']
+    },
+    reporters: ['spec'],
+    webpack: {
+      context: __dirname,
+      devtool: 'sourcemap',
+      module: {
+        rules: [
+          {
+            test: /\.html$/,
+            loaders: ['raw-loader']
+          },
+          {
+            test: /\.scss$/,
+            loaders: ['raw-loader', 'sass-loader']
+          },
+          {
+            test: /\.ts$/,
+            loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+          }
+        ]
+      },
+      plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.SourceMapDevToolPlugin({
+          filename: null,
+          test: /\.(ts|js)($|\?)/i
+        })
+      ],
+      resolve: {
+        extensions: ['.ts', '.js']
+      }
+    }
   });
 };
